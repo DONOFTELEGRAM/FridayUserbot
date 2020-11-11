1f131b5c7d02b14d8569fec61e8fba7d8356f2dd
VILLAIN <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-11-11 15:59:05 +0530

    Update pmpermit.py

--- fridaybot/modules/pmpermit.py
+++ fridaybot/modules/pmpermit.py
@@ -11,7 +11,7 @@ from fridaybot.Configs import Config
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-    WARN_PIC = "https://telegra.ph/file/53aed76a90e38779161b1.jpg"
+    WARN_PIC = "https://telegra.ph/file/fdd0297919732583732f4.jpg"
 else:
     WARN_PIC = PMPERMIT_PIC
 
